b3603a41bc3e27164a998dd888ce88081ae12890
shyamarshid <shyamkumar853@gmail.com>
2025-07-18 15:58:43 +0530

    Add skeleton C language support

---
+++ engine/src/main/java/com/ibm/engine/language/c/CxxBaseMethodVisitor.java
@@ -0,0 +1,10 @@
+package com.ibm.engine.language.c;
+
+import com.ibm.engine.detection.IBaseMethodVisitor;
+
+public class CxxBaseMethodVisitor implements IBaseMethodVisitor<Object> {
+    @Override
+    public void visitMethodDefinition(Object method) {
+        // no-op
+    }
+}
