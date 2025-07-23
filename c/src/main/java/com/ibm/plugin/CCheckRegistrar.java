7118b493dd66983b022a08847250f93ebebbed52
shyamarshid <shyamkumar853@gmail.com>
2025-07-23 17:14:08 +0530

    fix c module registrar

--- c/src/main/java/com/ibm/plugin/CCheckRegistrar.java
+++ c/src/main/java/com/ibm/plugin/CCheckRegistrar.java
@@ -2,16 +2,25 @@ package com.ibm.plugin;
 
 import java.util.List;
 import javax.annotation.Nonnull;
-import org.sonar.api.batch.rule.CheckRegistrar;
+import org.sonar.plugins.cxx.CustomCxxRulesDefinition;
 import org.sonarsource.api.sonarlint.SonarLintSide;
 
 @SonarLintSide
-
-public class CCheckRegistrar implements CheckRegistrar {
+public class CCheckRegistrar extends CustomCxxRulesDefinition {
     @Override
-    public void register(RegistrarContext registrarContext) {
-        registrarContext.registerClassesForRepository(
-                CScannerRuleDefinition.REPOSITORY_KEY, checkClasses(), testCheckClasses());
+    public String repositoryName() {
+        return CScannerRuleDefinition.REPOSITORY_NAME;
+    }
+
+    @Override
+    public String repositoryKey() {
+        return CScannerRuleDefinition.REPOSITORY_KEY;
+    }
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public Class[] checkClasses() {
+        return CRuleList.getChecks().toArray(new Class[0]);
     }
 
     public static @Nonnull List<Class<?>> checkClasses() {
