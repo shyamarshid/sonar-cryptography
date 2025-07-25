400d1d976918197bc154387f85b470fb7c4f0c7a
shyamarshid <shyamkumar853@gmail.com>
2025-07-25 08:48:49 +0530

    Update CScannerRuleDefinition.java added logging with import url and logger
    Signed-off-by: shyamarshid <shyamkumar853@gmail.com>

--- c/src/main/java/com/ibm/plugin/CScannerRuleDefinition.java
+++ c/src/main/java/com/ibm/plugin/CScannerRuleDefinition.java
@@ -1,38 +1,54 @@
 package com.ibm.plugin;
 
-import java.util.Objects;
+import java.net.URL;
 import org.sonar.api.SonarRuntime;
 import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.api.server.rule.RulesDefinition.NewRule;
 import org.sonar.plugins.cxx.CxxLanguage;
 import org.sonarsource.analyzer.commons.RuleMetadataLoader;
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
 
 public class CScannerRuleDefinition implements RulesDefinition {
-    public static final String REPOSITORY_KEY = "c";
-    public static final String REPOSITORY_NAME = "Sonar Cryptography";
-    private static final String RESOURCE_BASE_PATH = "/org/sonar/l10n/c/rules/c";
 
-    private final SonarRuntime sonarRuntime;
+  private static final Logger LOG = Loggers.get(CScannerRuleDefinition.class);
 
-    public CScannerRuleDefinition(SonarRuntime sonarRuntime) {
-        this.sonarRuntime = sonarRuntime;
+  public static final String REPOSITORY_KEY = "c";
+  public static final String REPOSITORY_NAME = "Sonar Cryptography";
+  // no leading slash
+  private static final String RESOURCE_BASE_PATH = "org/sonar/l10n/c/rules/c";
+
+  private final SonarRuntime sonarRuntime;
+
+  public CScannerRuleDefinition(SonarRuntime sonarRuntime) {
+    this.sonarRuntime = sonarRuntime;
+  }
+
+  @Override
+  public void define(Context context) {
+    // sanity check: are the metadata files on the classpath?
+    URL json = getClass().getClassLoader()
+        .getResource(RESOURCE_BASE_PATH + "/Inventory.json");
+    URL html = getClass().getClassLoader()
+        .getResource(RESOURCE_BASE_PATH + "/Inventory.html");
+    LOG.info("C rule metadata on classpath? json={}, html={}", json, html);
+
+    NewRepository repo =
+        context.createRepository(REPOSITORY_KEY, CxxLanguage.KEY)
+               .setName(REPOSITORY_NAME);
+
+    RuleMetadataLoader loader = new RuleMetadataLoader(RESOURCE_BASE_PATH, sonarRuntime);
+    loader.addRulesByAnnotatedClass(repo, CRuleList.getChecks());
+
+    // Hard fallback so SonarQube can't crash even if JSON wasn't picked up
+    for (NewRule r : repo.rules()) {
+      r.setName(r.key());
+    }
+    NewRule inv = repo.rule("Inventory");
+    if (inv != null) {
+      inv.setName("Cryptographic Inventory (CBOM)");
     }
 
-    @Override
-    public void define(Context context) {
-
-        URL json = getClass().getClassLoader().getResource(RESOURCE_BASE_PATH + "/Inventory.json");
-        URL html = getClass().getClassLoader().getResource(RESOURCE_BASE_PATH + "/Inventory.html");
-        LOG.info("C rule metadata: json={}, html={}", json, html);
-        NewRepository repository =
-                context.createRepository(REPOSITORY_KEY, CxxLanguage.KEY).setName(REPOSITORY_NAME);
-
-        RuleMetadataLoader loader = new RuleMetadataLoader(RESOURCE_BASE_PATH, this.sonarRuntime);
-        loader.addRulesByAnnotatedClass(repository, CRuleList.getChecks());
-        // Fallback: force-set the name for the rule(s) that were failing
-        NewRule inv = repository.rule("Inventory");
-        if (inv != null) {
-          inv.setName("Cryptographic Inventory (CBOM)");
-        }
-        repository.done();
-    }
+    repo.done();
+  }
 }
