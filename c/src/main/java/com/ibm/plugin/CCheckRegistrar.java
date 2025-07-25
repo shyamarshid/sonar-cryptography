c01b97194e21c8cc03433a9cc53fec6c356dbc4b
shyamarshid <shyamkumar853@gmail.com>
2025-07-25 10:20:42 +0530

    Update CCheckRegistrar.java to reflect scannerrule file changes
    Signed-off-by: shyamarshid <shyamkumar853@gmail.com>

--- c/src/main/java/com/ibm/plugin/CCheckRegistrar.java
+++ c/src/main/java/com/ibm/plugin/CCheckRegistrar.java
@@ -1,22 +1,32 @@
 package com.ibm.plugin;
+
+import java.util.List;
 import org.sonar.plugins.cxx.CustomCxxRulesDefinition;
 import org.sonarsource.api.sonarlint.SonarLintSide;
 
 @SonarLintSide
-public class CCheckRegistrar extends CustomCxxRulesDefinition {
-    @Override
-    public String repositoryName() {
-        return CScannerRuleDefinition.REPOSITORY_NAME;
-    }
+public final class CCheckRegistrar extends CustomCxxRulesDefinition {
 
-    @Override
-    public String repositoryKey() {
-        return CScannerRuleDefinition.REPOSITORY_KEY;
-    }
-
-    @Override
-    @SuppressWarnings("rawtypes")
-    public Class[] checkClasses() {
-        return CRuleList.getChecks().toArray(new Class[0]);
-    }
+  @Override
+  public String repositoryName() {
+    return CScannerRuleDefinition.REPOSITORY_NAME; // "Sonar Cryptography"
   }
+
+  @Override
+  public String repositoryKey() {
+    return CScannerRuleDefinition.REPOSITORY_KEY;  // "sonar-c-crypto"
+  }
+
+  @Override
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  public Class[] checkClasses() {
+    List<Class<?>> checks = CRuleList.getChecks();
+    return checks.toArray(new Class[0]);
+  }
+
+  @Override
+  @SuppressWarnings({"rawtypes"})
+  public Class[] testCheckClasses() {
+    return new Class[0];
+  }
+}
